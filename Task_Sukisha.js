f0501b29b2d329d30057f46922b04b342f9e1025
Sukisha6502 <152252208+Sukisha6502@users.noreply.github.com>
2024-01-04 10:29:28 +0530

    Add files via upload

--- Task_Sukisha.js
+++ Task_Sukisha.js
@@ -8,24 +8,35 @@
 // Finaly call the function with the four arguments as train number, from, to, day
 
 
-const arr1=[20658,"Ananthapuri Express",["Monday","Wednesday","Friday"]];
-const arr2=["Chennai","Vallioor",450,12];
-const [trainNumber,Name,[days]]=arr1;
-const [from,to,fare,hours]=arr2;
-function trainDetails(trainNum,from,to,day)
-{
-    const details=arr1.map((val) =>{
-       return val.trainNumber===trainNum && val.from===from && val.to===to && val.days.includes(day)
-    });
-    if(details){
-        return {Train_Number:trainNumber,Train_Name:Name,TrainRunning_Days:day,Fare:fare,Travel_Hours:hours };
+
+const arr1=[{trainNum:65248,trainName:"Ananthapuri Express",trainRunningDays:["Monday","Wednesday","Friday"]},
+            {trainNum:45290,trainName:"chennai Express",trainRunningDays:["Monday","Tuesday","Saturday"]},
+            {trainNum:16649,trainName:"Mumbai Express",trainRunningDays:["sunday","Wednesday","Thursday"]},
+            {trainNum:10211,trainName:"Guruvayur Express",trainRunningDays:["Sunday","Wednesday","Friday"]},
+            {trainNum:22542,trainName:"Kanyakumari Express",trainRunningDays:["Tuesday","Wednesday","Saturday"]}];
+const arr2=[{fromStation:"Chennai",toStation:"Vallioor",fare:469,travelHours:12},
+            {fromStation:"kanyakumari",toStation:"chennai",fare:480,travelHours:12},
+            {fromStation:"Tirunelveli",toStation:"mumbai",fare:690,travelHours:20},
+            {fromStation:"Nagercoil",toStation:"Thiruvandram",fare:300,travelHours:3}];
+function trainDetails(Number,from,to,day){
+    const train=arr1.find((ele) => (ele.trainNum===Number && ele.trainRunningDays.includes(day)));
+    if(train){
+        const station=arr2.find((ele) => (ele.fromStation===from && ele.toStation===to));
+    if(station){
+        return {Train_Number:train.trainNum,Train_Name:train.trainName,TrainRunning_Days:train.trainRunningDays,Fare:station.fare,Travel_Hours:station.travelHours };
     }
     else{
         return null;
     }
+} else{
+    return null
 }
+}
+console.log(trainDetails(65248,"Chennai","Vallioor","Wednesday"));
 
-console.log(trainDetails(20658,"Chennai","Vallioor","Wednesday"));
+
+
+
 
 
 
