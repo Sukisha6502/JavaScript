24abdd280e2590ae8daeed7cde03eb7c4deba2ee
Sukisha6502 <152252208+Sukisha6502@users.noreply.github.com>
2024-01-04 10:37:51 +0530

    Add files via upload

--- Task_Sukisha.js
+++ Task_Sukisha.js
@@ -9,30 +9,30 @@
 
 
 
-const arr1=[{trainNum:65248,trainName:"Ananthapuri Express",trainRunningDays:["Monday","Wednesday","Friday"]},
-            {trainNum:45290,trainName:"chennai Express",trainRunningDays:["Monday","Tuesday","Saturday"]},
-            {trainNum:16649,trainName:"Mumbai Express",trainRunningDays:["sunday","Wednesday","Thursday"]},
-            {trainNum:10211,trainName:"Guruvayur Express",trainRunningDays:["Sunday","Wednesday","Friday"]},
-            {trainNum:22542,trainName:"Kanyakumari Express",trainRunningDays:["Tuesday","Wednesday","Saturday"]}];
-const arr2=[{fromStation:"Chennai",toStation:"Vallioor",fare:469,travelHours:12},
-            {fromStation:"kanyakumari",toStation:"chennai",fare:480,travelHours:12},
-            {fromStation:"Tirunelveli",toStation:"mumbai",fare:690,travelHours:20},
-            {fromStation:"Nagercoil",toStation:"Thiruvandram",fare:300,travelHours:3}];
-function trainDetails(Number,from,to,day){
-    const train=arr1.find((ele) => (ele.trainNum===Number && ele.trainRunningDays.includes(day)));
-    if(train){
-        const station=arr2.find((ele) => (ele.fromStation===from && ele.toStation===to));
-    if(station){
-        return {Train_Number:train.trainNum,Train_Name:train.trainName,TrainRunning_Days:train.trainRunningDays,Fare:station.fare,Travel_Hours:station.travelHours };
+const arr1 = [{ trainNum: 65248, trainName: "Ananthapuri Express", trainRunningDays: ["Monday", "Wednesday", "Friday"] },
+{ trainNum: 45290, trainName: "chennai Express", trainRunningDays: ["Monday", "Tuesday", "Saturday"] },
+{ trainNum: 16649, trainName: "Mumbai Express", trainRunningDays: ["sunday", "Wednesday", "Thursday"] },
+{ trainNum: 10211, trainName: "Guruvayur Express", trainRunningDays: ["Sunday", "Wednesday", "Friday"] },
+{ trainNum: 22542, trainName: "Kanyakumari Express", trainRunningDays: ["Tuesday", "Wednesday", "Saturday"] }];
+const arr2 = [{ fromStation: "Chennai", toStation: "Vallioor", fare: 469, travelHours: 12 },
+{ fromStation: "kanyakumari", toStation: "chennai", fare: 480, travelHours: 12 },
+{ fromStation: "Tirunelveli", toStation: "mumbai", fare: 690, travelHours: 20 },
+{ fromStation: "Nagercoil", toStation: "Thiruvandram", fare: 300, travelHours: 3 }];
+function trainDetails(Number, from, to, day) {
+    const train = arr1.find((ele) => (ele.trainNum === Number && ele.trainRunningDays.includes(day)));
+    if (train) {
+        const station = arr2.find((ele) => (ele.fromStation === from && ele.toStation === to));
+        if (station) {
+            return { Train_Number: train.trainNum, Train_Name: train.trainName, TrainRunning_Days: train.trainRunningDays, Fare: station.fare, Travel_Hours: station.travelHours };
+        }
+        else {
+            return null;
+        }
+    } else {
+        return null
     }
-    else{
-        return null;
-    }
-} else{
-    return null
 }
-}
-console.log(trainDetails(65248,"Chennai","Vallioor","Wednesday"));
+console.log(trainDetails(65248, "Chennai", "Vallioor", "Wednesday"));
 
 
 
